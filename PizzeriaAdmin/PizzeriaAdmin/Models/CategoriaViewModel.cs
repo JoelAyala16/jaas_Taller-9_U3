f0f93eb4d82245e67ed15ddc10dde4df16d5aaea
JoelAyala16 <125949964+JoelAyala16@users.noreply.github.com>
2025-10-26 12:02:56 -0500

    Delete PizzeriaAdmin/PizzeriaAdmin directory

--- PizzeriaAdmin/PizzeriaAdmin/Models/CategoriaViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace PizzeriaAdmin.Models
-{
-    public class CategoriaViewModel
-    {
-        public int Id { get; set; }
-        public string Nombre { get; set; } = string.Empty;
-        public bool Activo { get; set; }
-    }
-}
